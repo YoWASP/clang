089a32b847f2e76ddac3de670e077ac517eac535
Catherine <whitequark@whitequark.org>
2024-05-18 18:32:58 +0000

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,101 @@
+#!/bin/sh -ex
+
+export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
+
+WASI_SDK=wasi-sdk-22.0
+WASI_SDK_URL=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-22/wasi-sdk-22.0-linux.tar.gz
+if ! [ -d ${WASI_SDK} ]; then curl -L ${WASI_SDK_URL} | tar xzf -; fi
+WASI_SDK_PATH=$(pwd)/${WASI_SDK}
+
+WASI_TARGET="wasm32-wasi"
+WASI_SYSROOT="--sysroot ${WASI_SDK_PATH}/share/wasi-sysroot"
+WASI_CFLAGS="" # "-flto"
+WASI_LDFLAGS="-O0" # "-flto -Wl,--strip-all"
+# LLVM doesn't build without <mutex>, etc, even with -DLLVM_ENABLE_THREADS=OFF.
+WASI_TARGET="${WASI_TARGET}-threads"
+WASI_CFLAGS="${WASI_CFLAGS} -pthread"
+WASI_LDFLAGS="${WASI_LDFLAGS} -Wl,--max-memory=4294967296"
+# LLVM assumes the existence of mmap.
+WASI_CFLAGS="${WASI_CFLAGS} -D_WASI_EMULATED_MMAN"
+WASI_LDFLAGS="${WASI_LDFLAGS} -lwasi-emulated-mman"
+
+cat >Toolchain-WASI.cmake <<END
+cmake_minimum_required(VERSION 3.4.0)
+
+set(WASI TRUE)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR wasm32)
+
+set(CMAKE_C_COMPILER ${WASI_SDK_PATH}/bin/clang)
+set(CMAKE_CXX_COMPILER ${WASI_SDK_PATH}/bin/clang++)
+set(CMAKE_LINKER ${WASI_SDK_PATH}/bin/wasm-ld CACHE STRING "wasienv build")
+set(CMAKE_AR ${WASI_SDK_PATH}/bin/ar CACHE STRING "wasienv build")
+set(CMAKE_RANLIB ${WASI_SDK_PATH}/bin/ranlib CACHE STRING "wasienv build")
+
+set(CMAKE_C_COMPILER_TARGET ${WASI_TARGET})
+set(CMAKE_CXX_COMPILER_TARGET ${WASI_TARGET})
+set(CMAKE_C_FLAGS "${WASI_SYSROOT} ${WASI_CFLAGS}" CACHE STRING "wasienv build")
+set(CMAKE_CXX_FLAGS "${WASI_SYSROOT} ${WASI_CFLAGS}" CACHE STRING "wasienv build")
+set(CMAKE_EXE_LINKER_FLAGS "${WASI_LDFLAGS}" CACHE STRING "wasienv build")
+set(CMAKE_EXECUTABLE_SUFFIX ".wasm")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+END
+
+if ! [ -f llvm-tblgen-build/bin/llvm-tblgen -a -f llvm-tblgen-build/bin/clang-tblgen ]; then
+  mkdir -p llvm-tblgen-build
+  cmake -B llvm-tblgen-build -S llvm-src/llvm \
+    -DLLVM_CCACHE_BUILD=ON \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DLLVM_BUILD_RUNTIME=OFF \
+    -DLLVM_BUILD_TOOLS=OFF \
+    -DLLVM_INCLUDE_UTILS=OFF \
+    -DLLVM_INCLUDE_RUNTIMES=OFF \
+    -DLLVM_INCLUDE_EXAMPLES=OFF \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF \
+    -DLLVM_INCLUDE_DOCS=OFF \
+    -DLLVM_TARGETS_TO_BUILD=WebAssembly \
+    -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-wasi \
+    -DLLVM_ENABLE_PROJECTS="clang" \
+    -DCLANG_BUILD_EXAMPLES=OFF \
+    -DCLANG_BUILD_TOOLS=OFF \
+    -DCLANG_INCLUDE_TESTS=OFF
+  cmake --build llvm-tblgen-build --target llvm-tblgen --target clang-tblgen
+fi
+
+mkdir -p llvm-build
+cmake -B llvm-build -S llvm-src/llvm \
+  -DCMAKE_TOOLCHAIN_FILE=../Toolchain-WASI.cmake \
+  -DLLVM_CCACHE_BUILD=ON \
+  -DLLVM_NATIVE_TOOL_DIR=$(pwd)/llvm-tblgen-build/bin \
+  -DCMAKE_BUILD_TYPE=MinSizeRel \
+  -DLLVM_ENABLE_ASSERTIONS=ON \
+  -DLLVM_BUILD_SHARED_LIBS=OFF \
+  -DLLVM_ENABLE_PIC=OFF \
+  -DLLVM_BUILD_STATIC=ON \
+  -DLLVM_ENABLE_THREADS=OFF \
+  -DLLVM_BUILD_RUNTIME=OFF \
+  -DLLVM_BUILD_TOOLS=OFF \
+  -DLLVM_INCLUDE_UTILS=OFF \
+  -DLLVM_BUILD_UTILS=OFF \
+  -DLLVM_INCLUDE_RUNTIMES=OFF \
+  -DLLVM_INCLUDE_EXAMPLES=OFF \
+  -DLLVM_INCLUDE_TESTS=OFF \
+  -DLLVM_INCLUDE_BENCHMARKS=OFF \
+  -DLLVM_INCLUDE_DOCS=OFF \
+  -DLLVM_TARGETS_TO_BUILD=WebAssembly \
+  -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-wasi \
+  -DLLVM_ENABLE_PROJECTS="clang;lld" \
+  -DCLANG_ENABLE_ARCMT=OFF \
+  -DCLANG_ENABLE_STATIC_ANALYZER=OFF \
+  -DCLANG_BUILD_TOOLS=OFF \
+  -DCLANG_BUILD_EXAMPLES=OFF \
+  -DCLANG_LINKS_TO_CREATE="clang;clang++" \
+  -DLLD_BUILD_TOOLS=OFF
+cmake --build llvm-build --target clang --target lld -j8
